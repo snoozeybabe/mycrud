705b7f9808ba5e0ada00e0d2105c26b725dec88c
snoozeybabe <dz.kiki15@gmail.com>
2018-08-15 23:12:23 +0200

    Ending form / Adding icons to rowtable

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -7,6 +7,31 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow  from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+function HomeIcon(props) {
+  return (
+    <SvgIcon {...props}  width="24" height="24" >
+    <path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/>
+    </SvgIcon>
+  );
+}
+function DeleteIcon(props) {
+  return (
+    <SvgIcon {...props}  width="18" height="18" >
+      <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z" />
+    </SvgIcon>
+  );
+}
+function ProfilIcon(props) {
+  return (
+    <SvgIcon {...props}  width="18" height="18" >
+    <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm0 4c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm6 12H6v-1.4c0-2 4-3.1 6-3.1s6 1.1 6 3.1V19z"/>
+    </SvgIcon>
+  );
+}
+
+
 
 const CustomCell = withStyles(theme => ({
   head : {
@@ -24,12 +49,16 @@ const styles = theme => ({
   root : {
     witdh : '100%',
     marginTop : theme.spacing.unit * 3,
-    overflowX : 'auto'
+    overflowX : 'auto',
   },
   table : {
     minWidth : 700,
   },
+  icon: {
+    padding : 'none' ,
+  },
   row : {
+    height : 40,
     '&:nth-of-type(odd)' : {
       backgrounColor : theme.palette.background.default,
     },
@@ -67,6 +96,11 @@ class ListUser extends Component {
         <CustomCell>{user.age}</CustomCell>
         <CustomCell>{user.n_dossier}</CustomCell>
         <CustomCell>{user.date_naissance}</CustomCell>
+        <CustomCell>
+          <ProfilIcon className={classes.icon} />
+          <HomeIcon className={classes.icon} />
+          <DeleteIcon className={classes.icon} />
+        </CustomCell>
       </TableRow>
     );
   }
@@ -87,6 +121,7 @@ class ListUser extends Component {
                 <CustomCell numeric >Age </CustomCell>
                 <CustomCell>N°Doss </CustomCell>
                 <CustomCell>Date Nai. </CustomCell>
+                <CustomCell></CustomCell>
               </TableRow>
             </TableHead>
             <TableBody>
--- src/containers/NewFormUser.js
+++ src/containers/NewFormUser.js
@@ -10,6 +10,7 @@ import Grid from '@material-ui/core/Grid';
 const styles = theme =>({
   root : {
     flexGrow : 1,
+    outline : 'none',
   },
   grid : {
     'backgroundColor' : '#BBDEFB' ,
@@ -32,44 +33,124 @@ class FormUser extends Component {
   constructor(props){
     super(props);
     this.state =  {
-      focused : true
+      gender      : '',
+      lastName    : '',
+      firstName   : '',
+      birthDay    : '',
+      nDossier    : '',
+      commentaire : ''
     };
+
+    this._handleSubmit = this._handleSubmit.bind(this);
+    this._handleInputOnChange = this._handleInputOnChange.bind(this);
   }
 
-  
+
+  insertUsers = datas =>{
+    if(!datas) return;
+      fetch("/users/add", {
+        method : 'POST',
+        headers: {'Content-type' : 'application/json'},
+        body   : JSON.stringify(datas)
+      })
+      .then(res => {
+          if(res.status >= 400) throw new Error("Bad server response");
+          //return res.json(); 
+      })
+      .then(function(datas) {
+        if(datas == "success") console.log("User added here ");
+      })
+      .catch(err => console.log(err));
+  }
+  _handleSubmit = e => {
+    this.insertUsers(this.state);
+    e.preventDefault();
+  }
+
+  _handleInputOnChange(e){
+    const target = e.target;
+    const name = target.name;
+    this.setState({
+      [name] : target.value
+    })
+  }
+
+
+
+
   render() {
     const { classes } = this.props;
 
     return (
-      
         <Grid className={classes.grid} container align="center" >
           <Grid item xs={12} >
-          <Form>
-            <FormGroup tag="fieldset" row className="col-sm-6" xs={6}>
+          <Form onSubmit={this._handleSubmit}>
+            <FormGroup tag="fieldset" row className="col-sm-12" xs={6}>
               <Container>
                 <Row className={classes.row}>
                   <Col md="12" className={classes.col}>
                     <Row className={classes.row} >
-                    <legend className="col-form-label col-sm-1"  style={{ paddingLeft: 0 }}>Civilité : </legend>
-                    <FormGroup check className="col-sm-1">
-                    <Label check>
-                      <Input type="radio" name="Mr"/>
-                      Mr
-                    </Label>
-                  </FormGroup>
-                <FormGroup check className="col-sm-1">
-                    <Label check>
-                      <Input type="radio" name="Mme"/>
-                      Mme
-                    </Label>
-                </FormGroup>
-                <FormGroup check className="col-sm-1">
-                    <Label check>
-                      <Input type="radio" name="Mlle"/>
-                      Mlle
-                    </Label>
-                  </FormGroup>
+                      <legend className="col-form-label col-sm-1"  style={{ paddingLeft: 0 }}>Civilité : </legend>
+                        <FormGroup check className="col-sm-1">
+                            <Label check>
+                              <Input type="radio" name="gender" value="Mr" onChange={this._handleInputOnChange}/>
+                              Mr
+                            </Label>
+                          </FormGroup>
+                        <FormGroup check className="col-sm-1">
+                            <Label check>
+                              <Input type="radio" name="gender" value="Mme" onChange={this._handleInputOnChange}/>
+                                Mme
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check className="col-sm-1">
+                          <Label check>
+                              <Input type="radio" name="gender" value="Mlle" onChange={this._handleInputOnChange}/>
+                                Mlle
+                            </Label>
+                        </FormGroup>
                     </Row>
+
+                    <Row className={classes.row}>
+                      <FormGroup row className="col-sm-12" >
+                        <Label>Nom</Label>
+                        <Input type="text" placeholder="Nom" name="lastName" id="lastName" onChange={this._handleInputOnChange}/>
+                      </FormGroup>
+                    </Row>
+
+                    <Row className={classes.row}>
+                      <FormGroup  row className="col-sm-12" >
+                        <Label>Prenom</Label>
+                        <Input type="text" placeholder="Prenom" name="firstName" id="firstName" onChange={this._handleInputOnChange}/>
+                      </FormGroup>
+                    </Row>
+
+                    <Row className={classes.row}>
+                      <FormGroup  row className="col-sm-4" >
+                        <Label>Date de naissance </Label>
+                        <Input type="date" placeholder="Date de naissance" name="birthDay" id="birthDay" onChange={this._handleInputOnChange}/>
+                      </FormGroup>
+                      <FormGroup row className="col-sm-5 offset-1" >
+                      <Label>N° Dossier</Label>
+                      <Input type="text" placeholder="N° Dossier" name="nDossier" id="nDossier" onChange={this._handleInputOnChange}/>
+                    </FormGroup>
+                    </Row>
+
+                    <Row className={classes.row}>
+                      <FormGroup row  className="col-sm-12">
+                        <Label>Commentaire</Label>
+                        <Input type="textarea" name="commentaire" id="commentaire" onChange={this._handleInputOnChange} />
+                      </FormGroup>
+                      </Row>
+
+                    <Row className={classes.row}>
+                      <FormGroup row className="col-sm-10" >
+                        <Col sm={{ size : 10, offset : 9}}>
+                          <Button color="primary">Ajouter</Button>{'  '}
+                        </Col>
+                      </FormGroup>
+                    </Row>
+
                   </Col>
                 </Row>
               </Container>
